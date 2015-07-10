c3afe87ece2fdef8a2f94c10f8de2d7c5400f5a1
akilawi <ahmad.ilaiwi@gmail.com>
2015-07-10 16:27:56 +0300

    finish signup page start to do the database connection

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var express = require('express'),
     config = require('./server/configure'),
     app = express();
+var mongoose = require('mongoose');
+var dbConfig = require('./db');
 
 
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 app = config(app);
+mongoose.connect(dbConfig.url);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Server up: http://localhost:' + app.get('port'));
 });
+
 
 //console.log(path);
 // catch 404 and forward to error handler
---
+++ db.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ahmadilaiwi on 7/10/15.
+ */
+module.exports = {
+    'url' : 'mongodb://localhost/Sa3ed'
+}
